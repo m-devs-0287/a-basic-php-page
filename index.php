7d8d9a74bb04c30f61c201dab51f2fa158d0c2ca
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-04-05 01:57:08 -0500

    creating a basic php page

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+  $username = "Ivy";
+  $greeting = 'Hello, ' . $username .'.';
+
+  $offer = [
+    'item' => 'Chocolate',
+    'qty' => 5,
+    'price' => 5,
+    'discount' => 4,
+  ];
+
+  $usual_price = $offer['qty'] * $offer['price'];
+  $offer_price = $offer['qty'] * $offer['discount'];
+  $saving = $usual_price - $offer_price;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Candy Store</title>
+</head>
+<body>
+  
+  <h1>The Candy Store</h1>
+  
+  <h2>Multi-buy Offer</h2>
+
+  <p><?= $greeting; ?></p>
+
+  <p class='stiker'>Save $<?= $saving?></p>
+
+  <p>
+    Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?>
+    for <?= $offer_price ?> <br> (usual price $<?= $usual_price ?>)
+  </p>
+
+
+</body>
+</html>
